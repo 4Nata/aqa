5d9017e45ac9ff1bdb65b632612ee83716807494
Natalie Marunchak <natalie@MacBook-Pro-Natalie.local>
2019-07-06 11:32:38 +0300

    Login Test_Negative Testing

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -4,20 +4,26 @@ import java.io.File;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import org.testng.reporters.jq.Main;
 
 public class LoginTest {
 
-	@Test
+	private WebDriver driver = null;
+
+	@Test // annotation which shows TestNG that this is a test described below
 
 	public void testUserIsAbleToLoginIntoApp() {
 
 		System.setProperty("webdriver.chrome.driver",
 				System.getProperty("user.dir") + File.separator + "drivers" + File.separator + "chromedriver");
 
-		WebDriver driver = new ChromeDriver();
+		this.driver = new ChromeDriver();
 
 		driver.get("https://www.saucedemo.com");
 
@@ -27,12 +33,130 @@ public class LoginTest {
 
 		driver.findElement(By.cssSelector("input[value='LOGIN']")).click();
 
-		String currentURL = driver.getCurrentUrl();
+		WebElement el = driver.findElement(By.id("shopping_cart_container"));
 
-		if (currentURL.equals("https://www.saucedemo.com/inventory.html")) {
-			System.out.println(currentURL + " - " + "User is successfully logged in");
+//		String currentURL = driver.getCurrentUrl();
+//
+//		if (currentURL.equals("https://www.saucedemo.com/inventory.html")) {
+//			System.out.println(currentURL + " - " + "User is successfully logged in");
+//
+//		}
 
-		}
+		SoftAssert soft = new SoftAssert();
+
+//		Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html");
+//		Assert.assertTrue(el.isDisplayed()); checked 1 by 1, if 1st assert is failed - the 2nd one won't be checked at all
+
+//		soft.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html"); 1st method and below 2nd method:
+		soft.assertTrue(driver.getCurrentUrl().equals("https://www.saucedemo.com/inventory.html"),
+				"Current URL after login should be 'https://www.saucedemo.com/inventory.html', " + "but fould ["
+						+ driver.getCurrentUrl() + "]");
+		soft.assertTrue(el.isDisplayed());
+		soft.assertAll(); // checked all asserts even when 1st one is failed, the next one still will be
+							// checked
+
+		driver.close();
+	}
+
+	@Test
+	public void testUserIsNotAbleToLoginWithoutUsernameAndPassword() {
+
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + File.separator + "drivers" + File.separator + "chromedriver");
+
+		this.driver = new ChromeDriver();
+
+		driver.get("https://www.saucedemo.com");
+		driver.findElement(By.cssSelector("input[value='LOGIN']")).click();
+		WebElement error = driver.findElement(By.cssSelector("h3[data-test='error']"));
+
+		SoftAssert soft = new SoftAssert();
+
+		soft.assertTrue(error.isDisplayed());
+		soft.assertEquals(error.getText(), "Epic sadface: Username is required");
+		soft.assertAll();
+		driver.close();
 
 	}
+	
+	@Test
+	public void testUserIsNotAbleToLoginWithoutUsernsme () {
+		
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + File.separator + "drivers" + File.separator + "chromedriver");
+
+		this.driver = new ChromeDriver();
+
+		driver.get("https://www.saucedemo.com");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.cssSelector("input[value='LOGIN']")).click();
+		
+		WebElement error = driver.findElement(By.cssSelector("h3[data-test='error']"));
+		
+		SoftAssert soft = new SoftAssert();
+		
+		soft.assertTrue(error.isDisplayed());
+		soft.assertEquals(error.getText(), "Epic sadface: Username is required");
+		soft.assertAll();
+		driver.close();
+		
+		
+	}
+	
+	@Test
+	public void testUserIsNotAbleToLoginWithoutPassword () {
+		
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + File.separator + "drivers" + File.separator + "chromedriver");
+
+		this.driver = new ChromeDriver();
+
+		driver.get("https://www.saucedemo.com");
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.cssSelector("input[value='LOGIN']")).click();
+		
+		WebElement error = driver.findElement(By.cssSelector("h3[data-test='error']"));
+		
+		SoftAssert soft = new SoftAssert();
+		
+		soft.assertTrue(error.isDisplayed());
+		soft.assertEquals(error.getText(), "Epic sadface: Password is required");
+		soft.assertAll();
+		driver.close();
+		
+		
+	}
+	
+	@Test
+	public void testLockedUserIsNotAbleToLogin () {
+		
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + File.separator + "drivers" + File.separator + "chromedriver");
+
+		this.driver = new ChromeDriver();
+
+		driver.get("https://www.saucedemo.com");
+		driver.findElement(By.id("user-name")).sendKeys("locked_out_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.cssSelector("input[value='LOGIN']")).click();
+		
+		WebElement error = driver.findElement(By.cssSelector("h3[data-test='error']"));
+		
+		SoftAssert soft = new SoftAssert();
+		
+		soft.assertTrue(error.isDisplayed());
+		soft.assertEquals(error.getText(), "Epic sadface: Sorry, this user has been locked out.");
+		soft.assertAll();
+		driver.close();
+	}
+
+	@AfterClass(alwaysRun = true) // AlwaysRun annotation occurs after all tests are finished in this class
+	// (doesn't matter if they are failed or passed)
+	public void closeBrowser() {
+
+		driver.quit();
+
+	}
+
 }
+
