149bfec41eea21f5b60363278e0053f3d3df5d25
Natalie Marunchak <natalie@MacBook-Pro-Natalie.local>
2019-07-29 20:24:00 +0300

    Final Update 07/29

--- src/test/java/tests/CheckoutFieldsValidation.java
+++ src/test/java/tests/CheckoutFieldsValidation.java
@@ -9,38 +9,31 @@ import pages.CheckoutStepOne;
 public class CheckoutFieldsValidation extends GenericTest {
 
 	@Test
-	public void testUserIsNotAbleToCompleteCheckoutWithEmptyFields() {
 
-		CheckoutStepOne checkout = openLoginPage().loginAs("standard_user", "secret_sauce").addProductToCart("Test.allTheThings() T-Shirt (Red)")
-				.openCart().startCheckout().checkoutFieldsValidation("", "", "");
+	public void validationCheckoutFields() {
+		CheckoutStepOne checkout1 = openLoginPage().loginAs("standard_user", "secret_sauce")
+				.addProductToCart("Test.allTheThings() T-Shirt (Red)").openCart().startCheckout()
+				.checkoutFieldsValidation("", "", "");
 
-		Assert.assertEquals(checkout.getErrorMessageText(), "Error: First Name is required");
-	}
+		Assert.assertEquals(checkout1.getErrorMessageText(), "Error: First Name is required");
 
-	public void testUserIsNotToCompleteCheckoutWithoutFirstName() {
+		CheckoutStepOne checkout2 = openLoginPage().loginAs("standard_user", "secret_sauce")
+				.addProductToCart("Sauce Labs Fleece Jacket").openCart().startCheckout()
+				.checkoutFieldsValidation("", "Lucky", "44441");
 
-		CheckoutStepOne checkout = openLoginPage().loginAs("standard_user", "secret_sauce").addProductToCart("Sauce Labs Fleece Jacket")
-				.openCart().startCheckout().checkoutFieldsValidation("", "Lucky", "44441");
+		Assert.assertEquals(checkout2.getErrorMessageText(), "Error: First Name is required");
 
-		Assert.assertEquals(checkout.getErrorMessageText(), "Error: First Name is required");
+		CheckoutStepOne checkout3 = openLoginPage().loginAs("standard_user", "secret_sauce")
+				.addProductToCart("Sauce Labs Onesie").openCart().startCheckout()
+				.checkoutFieldsValidation("Nata", "", "44441");
 
-	}
+		Assert.assertEquals(checkout3.getErrorMessageText(), "Error: Last Name is required");
 
-	public void testUserIsNotAbleToCompleteCheckoutWithoutLastName() {
+		CheckoutStepOne checkout4 = openLoginPage().loginAs("standard_user", "secret_sauce")
+				.addProductToCart("Sauce Labs Bike Light").openCart().startCheckout()
+				.checkoutFieldsValidation("Nata", "Lucky", "");
 
-		CheckoutStepOne checkout = openLoginPage().loginAs("standard_user", "secret_sauce").addProductToCart("Sauce Labs Onesie")
-				.openCart().startCheckout().checkoutFieldsValidation("Nata", "", "44441");
-
-		Assert.assertEquals(checkout.getErrorMessageText(), "Error: Last Name is required");
-
-	}
-
-	public void testUserIsNotAbleToCompleteCheckoutWithoutPostalCode() {
-
-		CheckoutStepOne checkout = openLoginPage().loginAs("standard_user", "secret_sauce").addProductToCart("Sauce Labs Bike Light")
-				.openCart().startCheckout().checkoutFieldsValidation("Nata", "Lucky", "");
-
-		Assert.assertEquals(checkout.getErrorMessageText(), "Error: Postal Code is required");
+		Assert.assertEquals(checkout4.getErrorMessageText(), "Error: Postal Code is required");
 
 	}
 
